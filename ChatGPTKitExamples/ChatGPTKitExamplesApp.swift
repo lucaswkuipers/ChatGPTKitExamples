039c8d570690def65356a21e9c773f9b2763403c
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2023-05-05 12:04:40 -0300

    Initial Commit

---
+++ ChatGPTKitExamples/ChatGPTKitExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTKitExamplesApp.swift
+//  ChatGPTKitExamples
+//
+//  Created by Lucas Werner Kuipers on 05/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTKitExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
